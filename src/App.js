cb539358c5c5afdd870689d28fa4f50f47cdec08
envalio <envalio@users.noreply.github.com>
2020-12-03 10:37:37 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import { questions as data, results } from './db/data';
 import './App.css';
 
 function App() {
+
+  const [questions, setQuetions] = useState(data);
+  const [resultVisible, handlResultVisible] = useState(false);
+
+  const handelChosenAnswer = (event) => {
+    const [id, score] = event.currentTarget.value.split('-');
+    setQuetions(questions.map(question => question.id === Number(id) ? ({ ...question, chosenScore: Number(score) }) : question))
+  }
+
+  const handlSubmit = () => {
+    handlResultVisible(true)
+  }
+
+  const handlReset = () => {
+    setQuetions(data);
+    handlResultVisible(false);
+  }
+
+  const sum = questions.reduce((acc, el) => acc + el.chosenScore, 0);
+
+  const result = results.find(result => sum <= result.maxRequiredScore && sum >= result.minRequiredScore);
+
+  const buttonDisabled = questions.some(question => !question.chosenScore);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className={'mainpage'}>
+      {questions.map(question => (
+        <div key={question.id} className={'formQuestion'}>
+          
+          <div className={'formTest'} id={`question ${question.id}`} className={'formQuestion'}>
+
+              <p>{question.title}</p>
+              {question.answer.map(answer => (
+                <div>
+                  <input 
+                    id={`${question.id}-${answer.id}`}
+                    type='radio' 
+                    name={`question ${question.id}`} 
+                    value={`${question.id}-${answer.score}`}
+                    onChange={handelChosenAnswer}
+                    checked={question.chosenScore === answer.score}
+                    className={'formQuestion'}  
+                  />
+                  <label for={`${question.id}-${answer.id}`}>{answer.title}</label>
+                </div>
+              ))}
+
+          </div>
+        </div>
+      ))}
+      <button onClick={handlSubmit} disabled={buttonDisabled}>
+        Get answer
+      </button>
+      {resultVisible && (
+        <div>
+          <div>
+            Result: {result ? result.title : ''}
+          </div>
+          <button onClick={handlReset}>
+            Reset answer
+          </button>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/db/data.js
@@ -0,0 +1,165 @@
+export const questions = [
+    {
+        id: 1,
+        title: 'Pick some food',
+        answer: [
+            {
+                id: 1,
+                title: "caesar salad",
+                score: 1
+            },
+            {
+                id: 2,
+                title: "nuggets",
+                score: 2
+            },
+            {
+                id: 3,
+                title: "burger",
+                score: 3
+            },
+            {
+                id: 4,
+                title: "chicken sandwich",
+                score: 4
+            }
+        ],
+        chosenScore: 0
+    },
+    {
+        id: 2,
+        title: 'Pick a healthy choice',
+        answer: [
+            {
+                id: 1,
+                title: "fruit",
+                score: 1
+            },
+            {
+                id: 2,
+                title: "salad",
+                score: 2
+            },
+            {
+                id: 3,
+                title: "parfait",
+                score: 3
+            },
+            {
+                id: 4,
+                title: "pasta salad",
+                score: 4
+            }
+        ],
+        chosenScore: 0
+    },
+    {
+        id: 3,
+        title: 'Pick a movie snack',
+        answer: [
+            {
+                id: 1,
+                title: "M&M’s",
+                score: 1
+            },
+            {
+                id: 2,
+                title: "caramel popcorn",
+                score: 2
+            },
+            {
+                id: 3,
+                title: "traditional popcorn",
+                score: 3
+            },
+            {
+                id: 4,
+                title: "ice cream",
+                score: 4
+            }
+        ],
+        chosenScore: 0
+    },
+    {
+        id: 4,
+        title: 'Pick a fast-food spot',
+        answer: [
+            {
+                id: 1,
+                title: "Dunkin’ Donuts",
+                score: 1
+            },
+            {
+                id: 2,
+                title: "Wendy’s",
+                score: 2
+            },
+            {
+                id: 3,
+                title: "Burger King",
+                score: 3
+            },
+            {
+                id: 4,
+                title: "McDonald’s",
+                score: 4
+            }
+        ],
+        chosenScore: 0
+    },
+    {
+        id: 5,
+        title: 'Pick some more food',
+        answer: [
+            {
+                id: 1,
+                title: "pasta",
+                score: 1
+            },
+            {
+                id: 2,
+                title: "chicken",
+                score: 2
+            },
+            {
+                id: 3,
+                title: "sandwich",
+                score: 3
+            },
+            {
+                id: 4,
+                title: "pizza",
+                score: 4
+            }
+        ],
+        chosenScore: 0
+    }
+];
+
+export const results = [
+    {
+        id: '1',
+        title: 'You’re a very serious person!',
+        minRequiredScore: 5,
+        maxRequiredScore: 8
+
+    },
+    {
+        id: '2',
+        title: 'You have a wicked sense of humour',
+        minRequiredScore: 9,
+        maxRequiredScore: 12
+    },
+    {
+        id: '3',
+        title: 'You’re a perfect mix of funny, chill, and intelligence!',
+        minRequiredScore: 13,
+        maxRequiredScore: 16
+    },
+    {
+        id: '4',
+        title: 'You’re shy and reserved!',
+        minRequiredScore: 17,
+        maxRequiredScore: 20
+    }
+]
